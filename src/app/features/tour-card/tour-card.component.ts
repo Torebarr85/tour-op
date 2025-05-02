5b14cd6df5a0d2f56f754f80da5a97b3e60ea0e1
Salvatore Barretta <yye5821@infocert.it>
2025-05-02 22:42:38 +0200

    ADD tailwind improve UI

--- src/app/features/tour-card/tour-card.component.ts
+++ src/app/features/tour-card/tour-card.component.ts
@@ -2,25 +2,21 @@ import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { Tour } from '../../interfaces/tour';
+import { MatIcon } from '@angular/material/icon';
 @Component({
   selector: 'app-tour-card',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatIcon],
   templateUrl: './tour-card.component.html',
   styleUrl: './tour-card.component.css',
 })
 export class TourCardComponent {
-
-
   @Input() tour: Tour = {
     id: 0,
-    name: '',
+    title: '',
     location: '',
     price: 0,
     imageUrl: '',
-    description: ''
+    description: '',
   };
-
-  
- 
 }
--- src/app/interfaces/tour.ts
+++ src/app/interfaces/tour.ts
@@ -1,6 +1,6 @@
 export interface Tour {
   id: number;
-  name: string;
+  title: string;
   location: string;
   price: number;
   imageUrl: string;
