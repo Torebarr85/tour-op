5c3e3fc4bdd52072935f35bbb89beedc23eb36f0
Salvatore Barretta <yye5821@infocert.it>
2025-05-02 19:34:10 +0200

    ADD http client, tours service, card

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,12 @@ import { provideRouter } from '@angular/router';
 import { provideAuth0 } from '@auth0/auth0-angular';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
     provideRouter(routes),
     provideAnimationsAsync('noop'),
     provideAuth0({
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Tour } from '../../interfaces/tour';
+import { TourService } from '../../services/tour.service';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { TourCardComponent } from '../tour-card/tour-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, JsonPipe, TourCardComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export default class HomeComponent {
+export default class HomeComponent implements OnInit {
+  tours$: Observable<Tour[]> = of([]);
+  tourService = inject(TourService);
 
+  ngOnInit(): void {
+    this.tours$ = this.tourService.getTours();
+  }
 }
