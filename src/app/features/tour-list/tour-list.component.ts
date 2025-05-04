49e0d6759c3c952f8e4dfd8e67e6a1d5dd0550c9
Salvatore Barretta <yye5821@infocert.it>
2025-05-04 19:06:56 +0200

    Search filter ultra reactive works

--- src/app/features/tour-list/tour-list.component.ts
+++ src/app/features/tour-list/tour-list.component.ts
@@ -1,19 +1,19 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Component, inject } from '@angular/core';
+import { combineLatest, map, Observable, of, startWith } from 'rxjs';
 import { Tour } from '../../interfaces/tour';
 import { TourService } from '../../services/tour.service';
-import { AsyncPipe, CommonModule, JsonPipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { TourCardComponent } from '../tour-card/tour-card.component';
 import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-tour-list',
   standalone: true,
   imports: [
-    AsyncPipe,
     TourCardComponent,
     MatFormFieldModule,
     CommonModule,
@@ -24,14 +24,33 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
   ],
   templateUrl: './tour-list.component.html',
 })
-export default class TourListComponent implements OnInit {
-  tours$: Observable<Tour[]> = of([]);
+export default class TourListComponent {
   tourService = inject(TourService);
   searchControl = new FormControl('');
 
-  constructor() {}
+  tours$: Observable<Tour[]> = this.tourService.getTours();
+  // formControl.valueChanges è un observable che emette il valore corrente
+  // e startWith('') garantisce valore iniziale vuoto
+  search$ = this.searchControl.valueChanges.pipe(startWith(''));
 
-  ngOnInit(): void {
-    this.tours$ = this.tourService.getTours();
+  filteredToursSignal = toSignal(
+    //combineLatest: ogni volta che uno dei due observable emette, dammi l’ULTIMO valore di entrambi.
+    // Ti dà sempre la coppia aggiornata più recente di [tours, search]
+    combineLatest([this.tours$, this.search$]).pipe(
+      //è destructuring -> prende i due valori dell’array e li mette in variabili comode
+      map(([tours, search]) => {
+        //?? dice: Se a sinistra è null/undefined -> usa il valore a destra. cosi evita i null e undefined
+        const query = (search ?? '').toLowerCase();
+
+        // filtro i tour in base alla query
+        return tours.filter((tour) => tour.title.toLowerCase().includes(query));
+      })
+    ),
+    // Valore iniziale per il signal, utile finché non arrivano i dati
+    { initialValue: [] }
+  );
+
+  clearSearch() {
+    this.searchControl.reset();
   }
 }
